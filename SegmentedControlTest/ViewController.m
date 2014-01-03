b1f1729c87c9858f1f2eed3a1983e40845b0c919
ChungYeul In <sldk86@naver.com>
2014-01-03 13:54:56 +0900

    처음 올린다
    처음 올린

--- SegmentedControlTest/ViewController.m
+++ SegmentedControlTest/ViewController.m
@@ -9,21 +9,60 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)chooseNumber:(id)sender;
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UISegmentedControl *segment;
+}
 
+- (void) handleBallSelection:(UISegmentedControl *)sender {
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            NSLog(@"야구!");
+            break;
+        case 1:
+            NSLog(@"축구!");
+            break;
+        case 2:
+            NSLog(@"배구!");
+            break;
+        case 3:
+            NSLog(@"테니스!");
+            break;
+        default:
+            break;
+    }
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //
+    UIImage *baseball = [UIImage imageNamed:@"Baseball.png"];
+    UIImage *football = [UIImage imageNamed:@"Football.png"];
+    UIImage *volleyball = [UIImage imageNamed:@"Volleyball.png"];
+    UIImage *tennisball = [UIImage imageNamed:@"Tennisball.png"];
+    NSArray *items = [NSArray arrayWithObjects:baseball, football, volleyball, tennisball, nil];
+    
+    segment = [[UISegmentedControl alloc] initWithItems:items];
+    segment.frame = CGRectMake(10, 100, 300, 40);
+    [self.view addSubview:segment];
+    
+    segment.selectedSegmentIndex = 2;
+    
+    // 변경이벤트
+    [segment addTarget:self action:@selector(handleBallSelection:) forControlEvents:UIControlEventValueChanged];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)chooseNumber:(id)sender {
+    UISegmentedControl *control = (UISegmentedControl *)sender;
+    NSString *selectedStr = [control titleForSegmentAtIndex:control.selectedSegmentIndex];
+    NSLog(@"Index : %d - Title : %@", control.selectedSegmentIndex, selectedStr);
+}
 @end
